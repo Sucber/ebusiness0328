6ec2023a791c0bfe03ee4c41b215e658162f93ed
sucber <xuholong968@outlook.com>
2018-08-01 11:29:03 +0800

    step-manager-module

---
+++ ebusiness-manager-web/src/test/java/com/atguigu/ebusiness/manager/web/EbusinessManagerWebApplicationTests.java
@@ -0,0 +1,16 @@
+package com.atguigu.ebusiness.manager.web;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class EbusinessManagerWebApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
